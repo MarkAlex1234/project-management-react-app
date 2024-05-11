265e328b2b4fef3d5e1200a2a66bf16cd83506bd
Mark Alexander <mark.alexander1314@gmail.com>
2024-05-11 18:20:29 +1000

    add/Input-component

--- src/components/AddProjectDialog.tsx
+++ src/components/AddProjectDialog.tsx
@@ -1,6 +1,7 @@
 import React, { Dispatch, ReactElement, SetStateAction, useState } from 'react';
 import Button from './Button';
 import { format } from 'date-fns';
+import Input from './Input';
 
 interface Props {
   open: boolean;
@@ -11,11 +12,6 @@ export default function AddProjectDialog({
   open,
   setOpenDialog,
 }: Props): ReactElement {
-  // Set today's date as the min value for the date input.
-  const today = new Date();
-  // Format today's date as YYYY-MM-DD for the min attribute of the input
-  const formattedToday = format(today, 'yyyy-MM-dd');
-
   function handleCancelClick() {
     setOpenDialog((prevValue) => !prevValue);
   }
@@ -30,7 +26,7 @@ export default function AddProjectDialog({
       className="backdrop:bg-stone-900/90 p-4 rounded-md shadow-md w-[35rem] mt-16"
       open={open}
     >
-      <div className="flex gap-4 flex-col">
+      <div className="flex flex-col">
         <menu className="flex items-center justify-end gap-4 my-4">
           <Button variant="alert" onClick={handleCancelClick}>
             Cancel
@@ -39,30 +35,10 @@ export default function AddProjectDialog({
             Save
           </Button>
         </menu>
-        <p className="flex flex-col gap-1 my-4">
-          <label className="text-sm font-bold uppercase text-stone-500">
-            Title
-          </label>
-          <input className="w-full p-1 border-b-2 rounded-sm border-stone-300 bg-stone-200 text-stone-600 focus:outline-none focus:border-stone-600" />
-        </p>
-        <p className="flex flex-col gap-1 my-4">
-          <label className="text-sm font-bold uppercase text-stone-500">
-            Description
-          </label>
-          <input
-            type="text"
-            className="w-full p-1 border-b-2 rounded-sm border-stone-300 bg-stone-200 text-stone-600 focus:outline-none focus:border-stone-600"
-          />
-        </p>
-        <p className="flex flex-col gap-1 my-4">
-          <label className="text-sm font-bold uppercase text-stone-500">
-            Due Date
-          </label>
-          <input
-            className="w-full p-1 border-b-2 rounded-sm border-stone-300 bg-stone-200 text-stone-600 focus:outline-none focus:border-stone-600"
-            type="date"
-            min={formattedToday}
-          />
+        <p className="flex flex-col gap-4 my-4">
+          <Input type="text" label="Title" />
+          <Input type="text" label="Description" />
+          <Input type="date" label="Due Date" />
         </p>
       </div>
     </dialog>
---
+++ src/components/Input.tsx
@@ -0,0 +1,30 @@
+import { format } from 'date-fns';
+import React, { HTMLInputTypeAttribute, ReactElement } from 'react';
+
+interface Props {
+  type: HTMLInputTypeAttribute;
+  label: string;
+  min?: string | number;
+}
+
+export default function Input({ type, min, label }: Props): ReactElement {
+  let minValue = min;
+
+  // If there is a Min sent and the type is of Date we need to format it here and pass to the input;
+  if (min && type === 'date') {
+    minValue = format(min, 'yyyy-MM-dd');
+  }
+
+  return (
+    <>
+      <label className="text-sm font-bold uppercase text-stone-500">
+        {label}
+      </label>
+      <input
+        className="w-full p-1 border-b-2 rounded-sm border-stone-300 bg-stone-200 text-stone-600 focus:outline-none focus:border-stone-600"
+        type={type}
+        min={minValue}
+      />
+    </>
+  );
+}
